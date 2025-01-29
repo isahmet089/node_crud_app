d8ef5ceb73cfb7b2c173146413c4c4b9d20cd8f4
isahmet089 <isahmet089@gmail.com>
2025-01-30 02:25:01 +0300

    Klosör Routing yapısı kuruldu.

---
+++ main.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// DATABASE CONNECTION
+
+mongoose.connect(process.env.DB_URI);
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Database connected"));
+
+// MIDDLEWARES
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({
+  secret:"my secret key",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use((req,res,next)=>{
+  res.locals.message=req.session.message;
+  delete req.session.message;
+  next();
+})
+// SET TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+// ROUTES
+app.use("/",require("./routes/routes"));
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server ${PORT} portunda çalışıyor.`);
+});
