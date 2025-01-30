d1fc026550ddebe360d7d90cfdd3a067a16768c4
isahmet089 <isahmet089@gmail.com>
2025-01-30 03:24:53 +0300

    Header ve Users add  yapısı eklendi.

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.render("index",{title:"Anasayfa"});
 });
+
+router.get("/add",(req,res)=>{
+    res.render("add",{title:"Kullanıcı Ekle"});
+})
+
 module.exports = router;
